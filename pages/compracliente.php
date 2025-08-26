07ff1b25c9cbda162d0adb52190d2b9f81e6a9cf
DAVI CHICARELLI PEREIRA <davi.cpereira4@senacsp.edu.br>
2025-08-26 16:30:27 -0300

    commit inicial

---
+++ pages/compracliente.php
@@ -0,0 +1,36 @@
+<?php
+$nome = $_GET["nome"];
+$valorcompra = filter_input(INPUT_GET, "compra", FILTER_VALIDATE_FLOAT);
+$tipocliente = $_GET["tipocliente"];
+
+if ($tipocliente == "vip") { 
+    $desconto = $valorcompra * 0.10;
+} else {
+    $desconto = 0;
+}
+
+$valorfinal = $valorcompra - $desconto;
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Compra Cliente</title>
+    <link rel="stylesheet" href="./../css/estilo.css">
+<!-- o .. server para sair da pasta -->
+</head>
+<body>
+    <h1>Valor da Compra:</h1>
+    <div id="resultado">
+    
+    <?php
+    echo "Nome do Cliente: " . $nome . "<br>";
+    echo "Valor da Compra: R$ " . number_format($valorcompra, 2, ',', '.') . "<br>";
+    echo "Valor a Pagar: R$ " . number_format($valorfinal, 2, ',', '.') . "<br>";
+    ?>
+
+    </div>
+
+</body>
